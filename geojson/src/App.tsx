56ee832726f804c27a2054a2cde9432252029010
Justin Zhang <justin.zhang.1@stonybrook.edu>
2023-02-06 20:35:49 -0500

    commiting current changes

--- geojson/src/App.tsx
+++ geojson/src/App.tsx
@@ -4,26 +4,45 @@ import './App.css';
 import { Button } from '@mui/material';
 import L from "leaflet";
 import { GeoJsonObject } from 'geojson';
+import { Opacity } from '@mui/icons-material';
 
 function App() {
   const [geoFile, setGeoFile] = useState<File>();
-  const [geoJson, setGeoJson] = useState<GeoJsonObject | GeoJsonObject[]>();
+  const [geoJson, setGeoJson] = useState<any>(); // GeoJsonObject | GeoJsonObject[]
+  const [map, setMap] = useState<L.Map>();
+  
+
+  function onEachFeature(feature:any, layer:any){
+    const countryName = feature.properties.name;
+    let marker = L.marker([feature.properties.label_x, feature.properties.label_y], {opacity: 0.01});
+    marker.bindTooltip(countryName);
+    console.log(map);
+    // marker.addTo(geoMap);
+    // console.log(countryName);
+    layer.bindPopup(countryName);
+  }
 
   function loadMap(){
-    console.log(geoJson);
-    let map = L.map('map').setView([39.74739, -105], 4);
+    // console.log(geoJson?.features);
+    console.log(1)
     var myStyle = {
-      "fillColor": 'white'
+      fillColor: 'white',
+      fillOpacity: 1,
+      color:'black',
+
   };
-    L.geoJSON(geoJson, {
-      style: myStyle
-    }).addTo(map)
+    if(map != undefined){
+      L.geoJSON(geoJson, {
+        style: myStyle,
+        onEachFeature: onEachFeature,
+        }).addTo(map)
+    }
   }
 
   async function handelFile(selectorFiles:FileList){
     setGeoFile(selectorFiles[0])
     setGeoJson(JSON.parse(await selectorFiles[0].text()));
-    
+    setMap(L.map('map').setView([0,0], 0));
   }
 
   return (
@@ -33,14 +52,9 @@ function App() {
         <input hidden accept="file" type="file" onChange={(e)=>handelFile(e.target.files!)}/>
       </Button>
       <Button onClick={()=>loadMap()}>
-        Check
+        View Map
       </Button>
-      <div 
-        id="map" 
-        // style={{backgroundColor:"black"}}
-      >
-
-      </div>
+      <div id="map"></div>
     </div>
   );
 }
