0617e78996feac08435b86fd86e84c5938f33e1f
Justin Zhang <justin.zhang.1@stonybrook.edu>
2023-02-06 20:43:22 -0500

    undid a change to fix a bug

--- geojson/src/App.tsx
+++ geojson/src/App.tsx
@@ -9,20 +9,21 @@ import { Opacity } from '@mui/icons-material';
 function App() {
   const [geoFile, setGeoFile] = useState<File>();
   const [geoJson, setGeoJson] = useState<any>(); // GeoJsonObject | GeoJsonObject[]
-  const [map, setMap] = useState<L.Map>();
+  // const [map, setMap] = useState<L.Map>();
   
 
   function onEachFeature(feature:any, layer:any){
     const countryName = feature.properties.name;
     let marker = L.marker([feature.properties.label_x, feature.properties.label_y], {opacity: 0.01});
     marker.bindTooltip(countryName);
-    console.log(map);
-    // marker.addTo(geoMap);
+    // console.log(map);
+    // marker.addTo(layer);
     // console.log(countryName);
     layer.bindPopup(countryName);
   }
 
   function loadMap(){
+    let map = L.map('map').setView([0,0], 0)
     // console.log(geoJson?.features);
     console.log(1)
     var myStyle = {
@@ -31,18 +32,16 @@ function App() {
       color:'black',
 
   };
-    if(map != undefined){
-      L.geoJSON(geoJson, {
-        style: myStyle,
-        onEachFeature: onEachFeature,
-        }).addTo(map)
-    }
+    L.geoJSON(geoJson, {
+      style: myStyle,
+      onEachFeature: onEachFeature,
+      }).addTo(map)
   }
 
   async function handelFile(selectorFiles:FileList){
     setGeoFile(selectorFiles[0])
     setGeoJson(JSON.parse(await selectorFiles[0].text()));
-    setMap(L.map('map').setView([0,0], 0));
+    // setMap();
   }
 
   return (
