2d997186e5ac5f5348642e866de222e527a76263
Justin Zhang <justin.zhang.1@stonybrook.edu>
2023-02-06 22:18:34 -0500

    committing changes

--- geojson/src/App.tsx
+++ geojson/src/App.tsx
@@ -9,11 +9,12 @@ import { MapContainer, GeoJSON, TileLayer, LayersControl } from 'react-leaflet'
 
 function App() {
   const [geoFile, setGeoFile] = useState<File>();
-  const [geoJson, setGeoJson] = useState<any>(); // GeoJsonObject | GeoJsonObject[]
+  const [geoJson, setGeoJson] = useState<GeoJsonObject>(); // GeoJsonObject | GeoJsonObject[]
   const myStyle = {
-    fillColor: 'white',
+    fillColor: 'yellow',
     fillOpacity: 1,
     color:'black',
+    weight: 1
   };
   // const [map, setMap] = useState<L.Map>();
   
@@ -23,12 +24,20 @@ function App() {
     const countryName = feature.properties.name;
     layer.bindPopup(countryName);
     // console.log(countryName);
-    // layer.on({
-    //   click: (event:any)=>{console.log("click")}
-    // })
+    layer.on({
+      click: (event:any)=>{
+        console.log("click")
+      },
+      mouseOver: (event:any)=>{
+        event.target.setStyle({
+          color: "blue",
+          fillColor: "black"
+        });
+      }
+    })
   }
 
-  function loadMap(){
+  // function loadMap(){
     // let map = L.map('map').setView([0,0], 0)
     // console.log(geoJson?.features);
     // console.log(1)
@@ -37,7 +46,7 @@ function App() {
     //   style: myStyle,
     //   onEachFeature: onEachFeature,
     //   }).addTo(map)
-  }
+  // }
 
   async function handelFile(selectorFiles:FileList){
     setGeoFile(selectorFiles[0])
@@ -51,9 +60,9 @@ function App() {
         Upload GeoJson File
         <input hidden accept="file" type="file" onChange={(e)=>handelFile(e.target.files!)}/>
       </Button>
-      <Button onClick={()=>loadMap()}>
+      {/* <Button onClick={()=>loadMap()}>
         View Map
-      </Button>
+      </Button> */}
       <MapContainer style={{ height: "70vh" }} center={[0, 0]} zoom={0} zoomControl={true}>
         {
           geoJson?<GeoJSON data={geoJson} style = {myStyle} onEachFeature={onEachFeature}></GeoJSON>:null
