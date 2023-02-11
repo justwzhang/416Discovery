8a1e0c430ca659fa4ef064b4416656f277ba726b
Dongha <61765890+Modem924@users.noreply.github.com>
2023-02-10 20:20:49 -0500

    new change

--- shapefile/src/App.tsx
+++ shapefile/src/App.tsx
@@ -1,12 +1,18 @@
 import JSZip from 'jszip';
 import shp from 'shpjs';
-import React, { useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { MapContainer, GeoJSON, TileLayer } from 'react-leaflet'
 import ShapeFile from './Components/ShapeFile';
 import "leaflet/dist/leaflet.css";
+import { GeoJsonObject, FeatureCollection } from 'geojson';
+import * as turf from '@turf/turf';
+import { useMap } from "react-leaflet";
+import { geoJson } from 'leaflet';
+import { Button, TextField } from '@mui/material';
 
 function App() {
   const [geoData, setGeoData] = useState<ArrayBuffer | null>(null);
+  const [newName, setName] = useState<String>(" ");
   const [propName, setPropName] = useState<any>(" ");
 
   const countryStyle = {
@@ -80,45 +86,53 @@ function App() {
     // __________LOAD ZIP FILE SWITCH
   }
 
+  var selected: any[] = [];
+  var indexName: any[] = [];
+
   function onEachCountry(country: any, layer: any) {
     findPropName(country.properties);
     const countryName = country.properties[propName];
     layer.bindPopup(countryName);
-
-    layer.options.fillOpacity = Math.random() * 0.4;
-    var selected: any[] = [];
     var mergebt = document.getElementById("mergebt") as HTMLButtonElement;
+    var sel = document.getElementById("sel") as any;
+    layer.options.fillOpacity = Math.random() * 0.4;
 
     layer.on({
       click: (e: any) => {
         const layer = e.target;
-        var map = layer;
+        var fa = document.getElementById('name') as any;
+        const stateName = e.target.feature.properties.NAME_1;
+        const stateId = e.target.feature.properties.ID_1;
+        fa.innerHTML = stateName + ":" + stateId;
 
-        //color selected subregion
-        if (selected.length > 0) {
-          map.closePopup();
-          selected.pop();
-          layer.setStyle({
-            fillOpacity: Math.random() * 0.4,
-            weight: 1,
-            color: 'black',
-            fillColor: 'yellow'
-          });
-          console.log(selected);
+        
+        if (selected.length == 0){
+          selected.push(layer);
+          indexName.push(stateId);
         } else {
-          selected.push(map);
-          layer.setStyle({
-            fillColor: "red",
-            fillOpacity: 0.7,
-            weight: 2,
-            color: "black",
-          });
-          console.log(selected);
-          map = {};
+          if (indexName[0] == stateId) {
+            indexName.shift();
+            selected.shift();
+          }
+          else if (indexName[1] == stateId) {
+            indexName.pop();
+            selected.pop();
+          }
+          else if (selected.length == 2) {
+            selected.length = 0;
+            indexName.length = 0;
+            selected.push(layer);
+            indexName.push(stateId);
+          }
+          else {
+            selected.push(layer);
+            indexName.push(stateId);
+          }
         }
+        sel.innerHTML = indexName;
 
         //enable or disable merge button depending on selection
-        if (selected.length > 0) {
+        if (selected.length > 1) {
           mergebt.disabled = false;
         } else {
           mergebt.disabled = true;
@@ -136,22 +150,44 @@ function App() {
       },
       mouseout: (e: any) => {
         const layer = e.target;
-        if (selected.length > 0) { }
-        else {
           layer.setStyle({
             fillOpacity: Math.random() * 0.4,
             weight: 1,
             color: 'black',
             fillColor: 'yellow'
-          });
-        }
+        })
       },
     });
   }
 
 
   function Merge() {
+    for (var i = 0; i < selected.length; ++i){
+      if(i == 0) {
+        var unions = selected[i].toGeoJSON();
+      } else {
+        unions = turf.union(unions, selected[i].toGeoJSON());
+      }
+    }
+  }
 
+  function handleChange(e:any) {
+    if(e.target.value == ""){
+      setName(" ");
+    }else{
+      setName(e.target.value);
+    }
+  }
+
+  function handleNameChange(e:any) {
+    if (e.key == "Enter"){
+      var changedname = e.target.value;
+      indexName.length = 0;
+      indexName.push(changedname);
+      var fa = document.getElementById('name') as any;
+      fa.innerHTML = "Changed Name is " + changedname;
+      indexName.length = 0;
+    }
   }
 
   return (
@@ -169,7 +205,21 @@ function App() {
           </MapContainer> : null
       }
       <button id="mergebt" disabled onClick={Merge}>Merge</button>
-    </div>
+     <div className ="namebar">
+     <table>
+     <tr>
+       <th>Current</th>
+       <td id='name'> </td>
+     </tr>
+     <tr>
+       <th>You choose</th>
+       <td id='sel'></td>
+     </tr>
+     </table>
+   </div>
+   <TextField label="Change Name" variant="outlined" value={newName} style={{width: "100%"}} onChange={handleChange} onKeyDown={handleNameChange}/>
+   </div>
+    
   )
 }
 
